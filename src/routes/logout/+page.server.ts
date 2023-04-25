3acf677bd8247e25e907dddd00f9d17afa758ca1
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-04-25 19:59:59 +0700

    Delete src directory

--- src/routes/logout/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { redirect } from '@sveltejs/kit'
-
-export const load = async ({ cookies }) => {
-    //bye bye token
-    cookies.set('token', '', { path: '/' })
-	throw redirect(302, '/')
-}
--- src/routes/resetpassword/+page.ts
+++
@@ -1,5 +0,0 @@
-export const load = async ({ url }) => {
-    const token = url.searchParams.get('token')
-    const id = url.searchParams.get('id')
-    return { token, id }
-}
