41d9bbdb64104eda926537c1692269648a49297a
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-05-09 18:08:30 +0700

    Update +layout.svelte

--- src/routes/(homepage)/+layout.svelte
+++ src/routes/(homepage)/+layout.svelte
@@ -1,41 +1,36 @@
 <script lang="ts">
+    import "../../app.css";
+    import { applyAction, enhance } from '$app/forms'
+	  import { invalidateAll } from '$app/navigation'
     export let data:any;
-</script>
-
-<div class="hero min-h-screen bg-base-200">
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        {#if data.user == false}
-        <h1 class="text-5xl font-bold mb-6">Staff Navigator Dashboard</h1>
-        <h1 class="text-5xl font-bold">Please Login</h1>
+  </script>
+  
+  <div class="navbar bg-base-100">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <label tabindex="0" class="btn btn-ghost btn-circle">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+        </label>
+        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          {#if data.user == false}
+          <li><a href="/" class="text-green-500 dark:text-green-400">Home</a></li>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+          {/if}
+          {#if data.user == true}
+          <li><a href="/" class="text-green-400">Home</a></li>
+          <li><a href="/map">Map</a></li>
+          <li><a href="/history">History</a></li>
+          <li><a class="text-red-400" href="/logout">Logout</a></li>
         {/if}
-        {#if data.user == true}
-        <h1 class="text-3xl md:text-5xl font-bold">Hi, <br/> {data.email}</h1>
-        <!-- <h1 class="text-5xl font-bold">Hi, {data.email}</h1> -->
-        {/if}
-        <p class="py-6"></p>
-        {#if data.user == false}
-        <button class="btn btn-primary">
-          <a href="/login">Login</a>
-      </button>
-      <button class="btn btn-primary">
-        <a href="/register">Register</a>
-    </button>
-        {/if}
-        {#if data.user == true}
-        <button class="btn btn-primary">
-          <a href="/map">Tracking Map</a>
-      </button>
-      <button class="btn btn-primary">
-        <a href="/history">Location History</a>
-    </button>
-        {/if}
+        </ul>
       </div>
+    </div>
+    <div class="navbar-center">
+      <a href="/" class="btn btn-ghost normal-case text-xl">Staff Navigator</a>
+    </div>
+    <div class="navbar-end">
     </div>
   </div>
 
-<style lang="postcss">
-    html {
-        background-color: bg-gray-900;
-    }
-</style>
+<slot></slot>
