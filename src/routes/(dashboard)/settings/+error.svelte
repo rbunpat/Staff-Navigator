bb1ce7b03f64c1f0bcb692615f12d0f27840d246
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-11-05 20:27:30 +0700

    Create +error.svelte

---
+++ src/routes/(dashboard)/settings/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { page } from '$app/stores';
+    import { browser } from '$app/environment';
+    if (browser) { // to prevent error window is not defined, because it's SSR
+    window.location.href = '/';
+}
+</script>
+
+<!-- <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">{$page.error.message}</h1>
+        <div>
+          <button class="btn btn-primary">
+            <a href="/">Home</a>
+        </button>
+        </div>
+
+      </div>
+    </div>
+  </div> -->
