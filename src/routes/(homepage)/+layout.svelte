a43e735c8f68ddff58893f79653440d487b3f3c0
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-11-05 20:39:49 +0700

    Update +layout.svelte

--- src/routes/(homepage)/+layout.svelte
+++ src/routes/(homepage)/+layout.svelte
@@ -1,122 +1,41 @@
 <script lang="ts">
-    import "../../app.css";
-    import { applyAction, enhance } from '$app/forms'
-	  import { invalidateAll } from '$app/navigation'
     export let data:any;
-    export let form:any;
+</script>
 
-    // export function logout() {
-    //   fetch('/logout?/logout', { method: 'POST' }).then(() => {
-    //     invalidateAll()
-    //   })
-    // }
-
-    export function toggleNavbar() {
-      document.getElementById("navbar-default").classList.toggle("hidden");
-    }
-
-    export const logoUrl: string = import.meta.env.VITE_LOGOURL;
-  </script>
-  
-
-<nav class="bg-white border-gray-200 dark:bg-gray-900">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="/" class="flex items-center">
-        <img src={logoUrl} class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Staff Navigator</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14" on:click={toggleNavbar}>
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    
-    {#if data.user == true}
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="/map" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Map</a>
-        </li>
-        <li>
-          <a href="/history" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">History</a>
-        </li>
-        <li>
-          <a href="/settings" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Settings</a>
-        </li>
-        <form
-        class="logout"
-        action="/logout?/logout"
-        method="POST"
-        use:enhance={() => {
-        return async ({ result }) => {
-            invalidateAll()
-            await applyAction(result)
-        }
-        }}
-        >
-        <li>
-          <button type="submit">
-            <a class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
-
-          </button>
-      
-        </li>
-        </form>
-      </ul>
-    </div>
-    {/if}
-        
-    {#if data.user == false}
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="/login" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
-        </li>
-        <li>
-          <a href="/register" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sign Up</a>
-        </li>
-      </ul>
-    </div>
-    {/if}
-
-
-  </div>
-</nav>
-
-  <!-- <div class="navbar bg-base-100">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <label tabindex="0" class="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-        </label>
-        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-          {#if data.user == false}
-          <li><a href="/" class="text-green-500 dark:text-green-400">Home</a></li>
-          <li><a href="/login">Login</a></li>
-          <li><a href="/register">Register</a></li>
-          {/if}
-          {#if data.user == true}
-          <li><a href="/" class="text-green-400">Home</a></li>
-          <li><a href="/map">Map</a></li>
-          <li><a href="/history">History</a></li>
-          <li><a href="/settings">Settings</a></li>
-          <li><a class="text-red-400" href="/logout">Logout</a></li>
+<div class="min-h-screen bg-base-200">
+    <div class="hero-content text-center">
+      <div class="max-w-md">
+        {#if data.user == false}
+        <h1 class="text-5xl font-bold mb-6">Staff Navigator Dashboard</h1>
+        <h1 class="text-5xl font-bold">Please Login</h1>
         {/if}
-        </ul>
+        {#if data.user == true}
+        <h1 class="text-3xl md:text-5xl font-bold">Hi, <br/> {data.email}</h1>
+        <!-- <h1 class="text-5xl font-bold">Hi, {data.email}</h1> -->
+        {/if}
+        <p class="py-6"></p>
+        {#if data.user == false}
+        <button class="btn btn-primary">
+          <a href="/login">Login</a>
+      </button>
+      <button class="btn btn-primary">
+        <a href="/register">Register</a>
+    </button>
+        {/if}
+        {#if data.user == true}
+        <button class="btn btn-primary">
+          <a href="/map">Tracking Map</a>
+      </button>
+      <button class="btn btn-primary">
+        <a href="/history">Location History</a>
+    </button>
+        {/if}
       </div>
     </div>
-    <div class="navbar-center">
-      <a href="/" class="btn btn-ghost normal-case text-xl">Staff Navigator</a>
-    </div>
-    <div class="navbar-end">
-    </div>
-  </div> -->
+  </div>
 
-<slot></slot>
+<style lang="postcss">
+    html {
+        background-color: bg-gray-900;
+    }
+</style>
