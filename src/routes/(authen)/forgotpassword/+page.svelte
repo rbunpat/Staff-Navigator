f46b92eb5cf109f31b0007c6315a3bd03046e76b
Rachatat Bunpat <82020386+rbunpat@users.noreply.github.com>
2023-12-13 19:02:26 +0700

    Update +page.svelte

--- src/routes/(authen)/forgotpassword/+page.svelte
+++ src/routes/(authen)/forgotpassword/+page.svelte
@@ -6,7 +6,7 @@
   export let logoUrl: string = import.meta.env.VITE_LOGOURL;
 </script>
 
-<section class="bg-gray-50 dark:bg-gray-900">
+<section class="bg-gray-50 dark:bg-gray-900 ">
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img class="w-8 h-8 mr-2" src={logoUrl} alt="logo">
@@ -30,7 +30,7 @@
             </div>
     
           {/if}
-          <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" method="POST" action="?/forgotpassword">
+          <form class="mt-4 space-y-4 lg:mt-5 md:space-y-5" method="POST" action="?/forgotpassword" use:enhance>
               <div>
                   <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
                   <input type="email" name="email" id="email" placeholder="name@company.com" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
